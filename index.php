62ecbc605f5b8b36f7baea17a9d80cf85698af6a
Saeed <sm9296467@gmail.com>
2023-07-17 19:46:12 +0300

    edit the css

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ class formPlugin
     {
         add_shortcode("sfa_contact_form", array($this, 'contact_form'));
         add_action('wp_enqueue_scripts', array($this, 'my_custom_script'));
+        add_action('wp_enqueue_scripts', array($this, 'my_custom_script'));
     }
 
     public function contact_form()
@@ -26,18 +27,20 @@ class formPlugin
          <form method="POST">
             <input type="hidden"  id="contact_type" value="email" class="form-control">
             <input type="hidden" id="status" value="1" class="form-control">
+            <div class="form-group-name">
+                <label for="name">Name: </label>
+                <input type="text" id="name" class="sfa-form-input">
+            </div>
+            
+            <div class="form-group-mobile">
+                <label for="mobile">Mobile: </label>
+                <input type="text" id="mobile" class="sfa-form-input">
+            </div>
 
-            <label for="name">Name: </label>
-            <input type="text" id="name" class="sfa-form-input">
-            <br />
-
-            <label for="mobile">Mobile: </label>
-            <input type="text" id="mobile" class="sfa-form-input">
-            <br />
-
-            <label for="email">E-mail: </label>
-            <input type="email" id="email" class="sfa-form-input" required>
-            <br />
+            <div class="form-group-email">
+                <label for="email">E-mail: </label>
+                <input type="email" id="email" class="sfa-form-input" required>
+            </div>
 
             <button type="button" name="submit" class="sfa-form-btn" id="send" onclick="savee()">Send </button>
         </form>
@@ -52,7 +55,9 @@ class formPlugin
         wp_enqueue_script('SFA-contact-axios', "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js");
         wp_enqueue_script('SFA-contact-sweetalert', "https://cdn.jsdelivr.net/npm/sweetalert2@11");
 
-        wp_enqueue_script('SFA-contact-form', plugins_url('main.js', __FILE__));
+        wp_enqueue_script('SFA-contact-form-js', plugins_url('main.js', __FILE__));
+
+        wp_enqueue_style('SFA-contact-form-css', plugins_url('style.css', __FILE__));
     }
 };
 
